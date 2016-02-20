69543ab6cdf28de7889143553266677aad0b9a39
Tankle L <Tankle_@hotmail.com>
2016-02-21 00:18:01 +0800

    fixed bug: Wrong output -> printing attributes

--- OfficeDocuCmdPrint/OfficeDocuCmdPrint.cpp
+++ OfficeDocuCmdPrint/OfficeDocuCmdPrint.cpp
@@ -209,6 +209,8 @@ int main(int argc, char* argv[])
 							ofile << " } #" << pUtf8 << "#";
 							SAFE_DELETE_ARR(pUtf8);
 						}
+						else
+							ofile << "}";
 
 						ofile << endl;
 					}
